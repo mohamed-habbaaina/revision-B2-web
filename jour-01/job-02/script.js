3643de8cfc8c18f96987b821f60a38a5a950bd58
@mohamed.habaina <mohamed.habaina@gmail.com>
2024-06-06 17:05:30 +0200

    Job 02, lenght array and display

---
+++ jour-01/job-02/script.js
@@ -0,0 +1,68 @@
+const articlesArray = [
+    { id: 1, title: 'Introduction to JavaScript',
+      content: 'JavaScript is a versatile programming language used for web development.',
+      createdAt: new Date('2024-01-01T10:00:00Z'),
+      updatedAt: new Date('2024-01-02T12:00:00Z')
+    },
+    { id: 2, title: 'Understanding CSS',
+      content: 'CSS is used to style HTML elements and make web pages look attractive.',
+      createdAt: new Date('2024-01-05T14:00:00Z'),
+      updatedAt: new Date('2024-01-06T16:00:00Z')
+    },
+    { id: 3, title: 'Getting Started with HTML',
+      content: 'HTML is the standard markup language for creating web pages.',
+      createdAt: new Date('2024-01-10T09:00:00Z'),
+      updatedAt: new Date('2024-01-11T11:00:00Z')
+    }
+];
+
+function formatDate(date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+}
+
+
+function limitContent(array) {
+    let htmlContent = '';
+    for (const article of array) {
+        let resumContent = '';
+        let charCount = 0;
+        // Limiter le contenu à 10 caractères
+        for (const char of article.content) {
+            if (charCount >= 10) {
+                break;
+            }
+            resumContent += char;
+            charCount++;
+        }
+        // Ajouter '...' si le contenu est tronqué
+        if (charCount >= 10) {
+            resumContent += '...';
+        }
+
+        const formattedCreatedAt = formatDate(article.createdAt);
+        const formattedUpdatedAt = formatDate(article.updatedAt);
+
+
+        // Générer le contenu HTML
+        htmlContent += `<h2>Title: ${article.title}</h2>`;
+        htmlContent += `<p>Content: ${resumContent}</p>`;
+        htmlContent += `<p> Created date: ${formattedCreatedAt}</p>`;
+        htmlContent += `<p> Updated date: ${formattedUpdatedAt}</p>`;
+
+        console.log(`Titre: ${article.title}`)
+        console.log(`Content: ${resumContent}`)
+        console.log(`Created date: ${formattedCreatedAt}`)
+        console.log(`Updated date: ${formattedUpdatedAt}`);
+    }
+
+    // Mettre à jour le contenu de l'élément HTML
+    const html = document.getElementById('root');
+    if (html) {
+        html.innerHTML = htmlContent;
+    }
+}
+
+limitContent(articlesArray);
